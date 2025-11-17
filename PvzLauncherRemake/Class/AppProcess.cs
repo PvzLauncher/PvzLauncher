6aa28a22ca55177d4f0dee6ac8b07fcca4cfc281
HuaMouRen <2825338723@qq.com>
2025-11-17 15:59:37 +0800

    feat(app-process): 添加全局Process

---
+++ PvzLauncherRemake/Class/AppProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvzLauncherRemake.Class
+{
+    public static class AppProcess
+    {
+        public static Process Process = new Process();
+    }
+}
