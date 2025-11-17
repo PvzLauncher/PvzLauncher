c385a9c1483c045db7346c4a492809cbe44a75e4
HuaMouRen <2825338723@qq.com>
2025-11-17 14:19:42 +0800

    feat(utils): 新增工具ConfigManager

---
+++ PvzLauncherRemake/Utils/ConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using HuaZi.Library.Json;
+using PvzLauncherRemake.Class;
+using PvzLauncherRemake.Class.JsonConfigs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvzLauncherRemake.Utils
+{
+    public static class ConfigManager
+    {
+        public static void SaveAllConfig()
+        {
+            Json.WriteJson(Path.Combine(AppInfo.ExecutePath, "config.json"), AppInfo.Config);
+        }
+
+        public static void ReadAllConfig()
+        {
+            AppInfo.Config = Json.ReadJson<JsonConfig.Index>(Path.Combine(AppInfo.ExecutePath, "config.json"));
+        }
+    }
+}
