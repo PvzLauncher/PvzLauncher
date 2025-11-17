738934d315d11e202871aae6bc228b57757c1fd7
HuaMouRen <2825338723@qq.com>
2025-11-17 13:32:00 +0800

    feat(utils): 添加工具GameManager

---
+++ PvzLauncherRemake/Utils/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using HuaZi.Library.Json;
+using PvzLauncherRemake.Class;
+using PvzLauncherRemake.Class.JsonConfig;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static PvzLauncherRemake.Class.AppLogger;
+
+namespace PvzLauncherRemake.Utils
+{
+    public static class GameManager
+    {
+        /// <summary>
+        /// 加载游戏列表
+        /// </summary>
+        public static async Task LoadGameList()
+        {
+            await Task.Run(() =>
+            {
+                logger.Info("开始加载游戏列表");
+                //清理
+                AppInfo.GameList.Clear();
+
+                //游戏文件夹
+                string[] games = Directory.GetDirectories(AppInfo.GameDirectory);
+
+                foreach (var game in games)
+                {
+                    string configPath = Path.Combine(game, ".pvzl.json");
+                    if (File.Exists(configPath))
+                    {
+                        JsonGameInfo.Index configContent = Json.ReadJson<JsonGameInfo.Index>(configPath);
+                        logger.Info($"找到游戏配置: {configContent.GameInfo.Name}");
+                        AppInfo.GameList.Add(configContent);
+                    }
+                }
+
+                logger.Info("加载游戏列表结束");
+            });
+            
+        }
+    }
+}
