77f2cec4a57a5d81375d5ad6e942de06448f9ec4
HuaMouRen <2825338723@qq.com>
2025-11-17 14:08:21 +0800

    ref(json-configs): `JsonConfig` 更名 `JsonConfigs`

--- PvzLauncherRemake/Class/JsonConfigs/JsonGameInfo.cs
+++ PvzLauncherRemake/Class/JsonConfigs/JsonGameInfo.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PvzLauncherRemake.Class.JsonConfig
+namespace PvzLauncherRemake.Class.JsonConfigs
 {
     public class JsonGameInfo
     {
