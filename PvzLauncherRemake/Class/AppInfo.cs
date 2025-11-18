329937775b94a727275a21de29574f89984158e5
HuaMouRen <2825338723@qq.com>
2025-11-18 16:41:21 +0800

    feat(app-info): 添加全局变量DownloadIndexUrl

--- PvzLauncherRemake/Class/AppInfo.cs
+++ PvzLauncherRemake/Class/AppInfo.cs
@@ -13,7 +13,8 @@ namespace PvzLauncherRemake.Class
         public static readonly string GameDirectory = $"{Path.Combine(ExecutePath, "Games")}";//游戏目录
         public static List<JsonGameInfo.Index> GameList = new List<JsonGameInfo.Index>();//游戏列表
         public static JsonConfig.Index Config = null!;//配置
-        
+        public static readonly string DownloadIndexUrl = "https://gitee.com/huamouren110/UpdateService/raw/main/PvzLauncherRemake/download.json";//下载索引
+
         //启动参数配置
         public static class Arguments
         {
