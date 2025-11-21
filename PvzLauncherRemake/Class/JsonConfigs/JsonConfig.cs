52d97879aba076ae4ecd74701b4b6d8bb833e75a
HuaMouRen <2825338723@qq.com>
2025-11-21 12:31:26 +0800

    feat(json-config): 添加设置属性以及默认值

--- PvzLauncherRemake/Class/JsonConfigs/JsonConfig.cs
+++ PvzLauncherRemake/Class/JsonConfigs/JsonConfig.cs
@@ -12,7 +12,22 @@ namespace PvzLauncherRemake.Class.JsonConfigs
         public class Index
         {
             [JsonProperty("current_game")]
-            public string CurrentGame { get; set; }
+            public string CurrentGame { get; set; } = null!;
+
+            [JsonProperty("launcher_config")]
+            public LauncherConfig LauncherConfig { get; set; } = new LauncherConfig();
+        }
+
+        public class LauncherConfig
+        {
+            [JsonProperty("launched_operate")]
+            public string LaunchedOperate { get; set; } = "None";
+
+            [JsonProperty("window_title")]
+            public string WindowTitle { get; set; } = "Plants Vs. Zombies Launcher";
+
+            [JsonProperty("title_image")]
+            public string TitleImage { get; set; } = "EN";
         }
     }
 }
