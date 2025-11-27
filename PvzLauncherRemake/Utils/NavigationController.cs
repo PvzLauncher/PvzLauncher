5793a256b02c99a98bf9409e99f39edb3299d0eb
HuaMouRen <2825338723@qq.com>
2025-11-27 20:46:27 +0800

    feat(navigation-controller): 添加日志保护

--- PvzLauncherRemake/Utils/NavigationController.cs
+++ PvzLauncherRemake/Utils/NavigationController.cs
@@ -1,5 +1,6 @@
 ﻿using ModernWpf.Controls;
 using System.Windows;
+using static PvzLauncherRemake.Class.AppLogger;
 
 namespace PvzLauncherRemake.Utils
 {
@@ -10,7 +11,10 @@ namespace PvzLauncherRemake.Utils
             if (Window.GetWindow(obj) is MainWindow window)
                 if (window.FindName("navView") is NavigationView navView)
                     if (navView.FindName($"navViewItem_{target}") is NavigationViewItem navViewItem)
+                    {
+                        logger.Info($"[导航视图控制器] 导航到 \"{target}\"");
                         navView.SelectedItem = navViewItem;
+                    }
                     else
                         throw new Exception("找不到目标项");
                 else
