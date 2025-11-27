870f59dc543f377650c8dbe882ce7ef041fc977d
HuaMouRen <2825338723@qq.com>
2025-11-27 20:31:22 +0800

    feat(config-manager): 添加日志保护

--- PvzLauncherRemake/Utils/ConfigManager.cs
+++ PvzLauncherRemake/Utils/ConfigManager.cs
@@ -2,6 +2,7 @@
 using PvzLauncherRemake.Class;
 using PvzLauncherRemake.Class.JsonConfigs;
 using System.IO;
+using static PvzLauncherRemake.Class.AppLogger;
 
 namespace PvzLauncherRemake.Utils
 {
@@ -10,11 +11,13 @@ namespace PvzLauncherRemake.Utils
         public static void SaveAllConfig()
         {
             Json.WriteJson(Path.Combine(AppInfo.ExecuteDirectory, "config.json"), AppInfo.Config);
+            logger.Info($"[配置管理器] 保存所有配置");
         }
 
         public static void ReadAllConfig()
         {
             AppInfo.Config = Json.ReadJson<JsonConfig.Index>(Path.Combine(AppInfo.ExecuteDirectory, "config.json"));
+            logger.Info($"[配置管理器] 读取所有配置");
         }
     }
 }
