0643b46b0076014aac94f3f36068a0475eb3bbdf
HuaMouRen <2825338723@qq.com>
2025-11-28 10:48:03 +0800

    feat(user-card): 完成逻辑部分

--- PvzLauncherRemake/Controls/UserCard.xaml.cs
+++ PvzLauncherRemake/Controls/UserCard.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +21,87 @@ namespace PvzLauncherRemake.Controls
     /// </summary>
     public partial class UserCard : UserControl
     {
+        public string Title { get; set; } = "Title";
+        public string Description { get; set; } = "Description";
+        public string Icon { get; set; } = "origin";
+        public string Version { get; set; }
+        public string Size { get; set; }
+        public string SupportVersion { get; set; }
+        public bool isRecommend { get; set; }
+        public bool isNew { get; set; }
+        public bool isActive { get; set; }
+
         public UserCard()
         {
             InitializeComponent();
+            Loaded += ((s, e) =>
+            {
+                textBlock_Title.Text = Title;
+                textBlock_Description.Text = Description;
+
+                //图标
+                Viewbox[] icons = { viewBox_Origin, viewBox_Beta, viewBox_Tat };
+                foreach (var icon in icons)
+                {
+                    if ($"viewBox_{Icon}" == icon.Name)
+                        icon.Visibility = Visibility.Visible;
+                    else
+                        icon.Visibility = Visibility.Hidden;
+                }
+
+                SetLabels();
+            });
+        }
+
+        public void SetLabels()
+        {
+            //清除
+            stackPanel_Labels.Children.Clear();
+            if (!string.IsNullOrEmpty(Version))
+            {
+                string xaml =
+                    "<Grid Margin=\"0,0,5,0\">" +
+                        "<Rectangle Height=\"20\" RadiusY=\"3\" RadiusX=\"3\" Fill=\"#CC969696\"/>" +
+                        $"<TextBlock Text=\"{Version}\" HorizontalAlignment=\"Center\" VerticalAlignment=\"Center\" Foreground=\"White\" Margin=\"5,0,5,0\"/>" +
+                    "</Grid>";
+                stackPanel_Labels.Children.Add(XamlReader.Parse(xaml) as Grid);
+            }
+            if (!string.IsNullOrEmpty(Size))
+            {
+                string xaml =
+                    "<Grid Margin=\"0,0,5,0\">" +
+                        "<Rectangle Height=\"20\" RadiusY=\"3\" RadiusX=\"3\" Fill=\"#CC009696\"/>" +
+                        $"<TextBlock Text=\"{Size}\" HorizontalAlignment=\"Center\" VerticalAlignment=\"Center\" Foreground=\"White\" Margin=\"5,0,5,0\"/>" +
+                    "</Grid>";
+                stackPanel_Labels.Children.Add(XamlReader.Parse(xaml) as Grid);
+            }
+            if (!string.IsNullOrEmpty(SupportVersion))
+            {
+                string xaml =
+                    "<Grid Margin=\"0,0,5,0\">" +
+                        "<Rectangle Height=\"20\" RadiusY=\"3\" RadiusX=\"3\" Fill=\"#CC0000FF\"/>" +
+                        $"<TextBlock Text=\"支持版本: {SupportVersion}\" HorizontalAlignment=\"Center\" VerticalAlignment=\"Center\" Foreground=\"White\" Margin=\"5,0,5,0\"/>" +
+                    "</Grid>";
+                stackPanel_Labels.Children.Add(XamlReader.Parse(xaml) as Grid);
+            }
+            if (isRecommend)
+            {
+                string xaml =
+                    "<Grid Margin=\"0,0,5,0\">" +
+                        "<Rectangle Height=\"20\" RadiusY=\"3\" RadiusX=\"3\" Fill=\"#CC00FF00\"/>" +
+                        $"<TextBlock Text=\"推荐\" HorizontalAlignment=\"Center\" VerticalAlignment=\"Center\" Foreground=\"White\" Margin=\"5,0,5,0\"/>" +
+                    "</Grid>";
+                stackPanel_Labels.Children.Add(XamlReader.Parse(xaml) as Grid);
+            }
+            if (isActive)
+            {
+                string xaml =
+                    "<Grid Margin=\"0,0,5,0\">" +
+                        "<Rectangle Height=\"20\" RadiusY=\"3\" RadiusX=\"3\" Fill=\"#CCFF0000\"/>" +
+                        $"<TextBlock Text=\"活动\" HorizontalAlignment=\"Center\" VerticalAlignment=\"Center\" Foreground=\"White\" Margin=\"5,0,5,0\"/>" +
+                    "</Grid>";
+                stackPanel_Labels.Children.Add(XamlReader.Parse(xaml) as Grid);
+            }
         }
     }
 }
