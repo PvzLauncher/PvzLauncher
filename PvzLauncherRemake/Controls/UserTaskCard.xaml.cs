42264b68fc2248a49a510a21f89cbd1febf858cf
HuaMouRen <2825338723@qq.com>
2025-12-14 13:49:11 +0800

    feat(user-task-card): 支持显示解压进度

--- PvzLauncherRemake/Controls/UserTaskCard.xaml.cs
+++ PvzLauncherRemake/Controls/UserTaskCard.xaml.cs
@@ -20,6 +20,7 @@ namespace PvzLauncherRemake.Controls
     {
         public string Title { get; set; } = "Title";
         public double Progress { get; set; }
+        public double ProgressCompress { get; set; }
         public double Speed { get; set; }
 
 
@@ -31,10 +32,17 @@ namespace PvzLauncherRemake.Controls
         public void UpdateControl()
         {
             textBlock_Title.Text = Title;
-            textBlock_Progress.Text = Progress == 100 ? "解压中..." : $"{Math.Round(Progress, 2)}%";
-            textBlock_Speed.Text = $"{Math.Round(Speed,2)}Mb/s";
-            progressBar.Value = Progress;
+
+            textBlock_Progress_Download.Text = Progress == 100 ? "下载完成" : $"下载中... {Math.Round(Progress, 2)}%";
+            textBlock_Speed_Download.Text = $"{Math.Round(Speed,2)}Mb/s";
+            progressBar_Download.Value = Progress;
+
+            textBlock_Progress_Compress.Text = ProgressCompress == 100 ? "解压完毕" : ProgressCompress == 0 ? "等待下载完毕" : $"解压中... {Math.Round(ProgressCompress, 2)}%";
+            progressBar_Compress.Value = ProgressCompress;
+            
             button_Cancel.IsEnabled = Progress == 100 ? false : true;
+            progressRing_Download.IsActive = (Progress == 100 || Progress == 0) ? false : true;
+            progressRing_Compress.IsActive = (ProgressCompress == 100 || ProgressCompress == 0) ? false : true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
