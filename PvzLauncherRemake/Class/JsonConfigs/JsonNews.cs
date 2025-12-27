498382cf900c5c252cbc76d26f03a001107229fa
HuaMouRen <2825338723@qq.com>
2025-12-27 22:12:29 +0800

    feat(json-news): 添加新闻Json配置文件

---
+++ PvzLauncherRemake/Class/JsonConfigs/JsonNews.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PvzLauncherRemake.Class.JsonConfigs
+{
+    public class JsonNews
+    {
+        public class Index
+        {
+            [JsonProperty("news")]
+            public NewsInfo[] News { get; set; }
+        }
+
+        public class NewsInfo
+        {
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("date")]
+            public string Date { get; set; }
+
+            [JsonProperty("content")]
+            public string Content { get; set; }
+        }
+    }
+}
