1e094190b821654d6ae6032eac06566caa058e62
HuaMouRen <2825338723@qq.com>
2026-03-26 20:36:20 +0800

    ref(page-developer): 重置界面

--- PvzLauncherRemake/Pages/PageDeveloper.xaml.cs
+++ PvzLauncherRemake/Pages/PageDeveloper.xaml.cs
@@ -16,17 +16,11 @@ namespace PvzLauncherRemake.Pages
     /// </summary>
     public partial class PageDeveloper : ModernWpf.Controls.Page
     {
-        private bool isInitialize = false;
-
-        public async void MainCycle()
+        public void Initialize()
         {
-            logger.Info($"[开发者控制面板:周期循环] 开始进入周期循环");
-            while (true)
+            try
             {
-                await Task.Delay(1000);
-
-                // =====
-
+                #region 变量指标
                 string varText = "";
 
                 Type type = typeof(AppGlobals);
@@ -49,7 +43,25 @@ namespace PvzLauncherRemake.Pages
                     varText = $"{varText}{name}({typeName}): {value}\n\n";
                 }
 
-                textBlock_Variables.Text = varText;
+                textblock_varinfos.Text = varText;
+                #endregion
+
+                #region 导航
+                comboBox_navigator.Items.Clear();
+                foreach (var page in Enum.GetNames(typeof(NavigaionPages)))
+                    comboBox_navigator.Items.Add(page);
+                if (comboBox_navigator.Items.Count > 0) comboBox_navigator.SelectedIndex = 0;
+
+                button_navigator.Click += ((s, e) =>
+                {
+                    if (Enum.TryParse<NavigaionPages>((string)comboBox_navigator.SelectedItem, out var result))
+                        NavigationController.Navigate(result);
+                });
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                ErrorReportDialog.Show(ex);
             }
         }
 
@@ -57,68 +69,7 @@ namespace PvzLauncherRemake.Pages
         public PageDeveloper()
         {
             InitializeComponent();
-            Loaded += (async (s, e) =>
-            {
-                isInitialize = true;
-                logger.Info($"[开发者控制面板] 完成初始化!");
-            });
-            MainCycle();
-
-        }
-
-        private void textBox_markdown_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            markdownViewer.Markdown = textBox_markdown.Text;
-        }
-
-        private void textBox_Md2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (isInitialize)
-            {
-                var markdown = new MdXaml.Markdown();
-
-                FlowDocument doc = markdown.Transform(textBox_Md2.Text);
-
-                doc.FontFamily = new FontFamily("Microsoft Yahei UI");
-
-                foreach (Paragraph p in doc.Blocks.OfType<Paragraph>())
-                {
-                    p.LineHeight = 10;
-                    p.LineStackingStrategy = LineStackingStrategy.BlockLineHeight;
-                }
-
-                flowDocumentScrollViewer_Md2.Document = doc;
-            }
-        }
-
-        private async void button_Dialog_Show_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                async void ShowDialog(ContentDialog dialog)
-                {
-                    await DialogManager.ShowDialogAsync(dialog);
-                }
-                int count = 0;
-                switch ((string)button.Tag)
-                {
-                    case "1": count = 1; break;
-                    case "5": count = 5; break;
-                    case "10": count = 10; break;
-                    case "50": count = 50; break;
-                }
-
-                for (int i = 0; i < count; i++)
-                {
-                    ShowDialog(new ContentDialog
-                    {
-                        Title = textBox_Dialog_Title.Text,
-                        Content = textBox_Dialog_Content.Text,
-                        PrimaryButtonText = textBox_Dialog_Button.Text,
-                        DefaultButton = ContentDialogButton.Primary
-                    });
-                }
-            }
+            Loaded += ((s, e) => Initialize());
         }
     }
 }
