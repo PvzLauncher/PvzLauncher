49605c8c80d06000420e0ca9af000e5cbdfe2e5f
HuaMouRen <2825338723@qq.com>
2026-04-03 18:13:04 +0800

    imp(localize-manager): 优化逻辑

--- PvzLauncherRemake/Utils/Configuration/LocalizeManager.cs
+++ PvzLauncherRemake/Utils/Configuration/LocalizeManager.cs
@@ -7,7 +7,24 @@ namespace PvzLauncherRemake.Utils.Configuration
 {
     public class LocalizeManager
     {
+        public string Directory { get; set; }
+
         private static readonly string AssemblyName = Assembly.GetExecutingAssembly().GetName().Name!;
+
+
+        public string GetLoc(string key)
+        {
+            string fullKey = $"{AssemblyName}:{Directory}:{key}";
+            string value = LocExtension.GetLocalizedValue<string>(fullKey);
+            return value ?? fullKey;
+        }
+
+        public static string GetLoc(string directory, string key)
+        {
+            string fullKey = $"{AssemblyName}:{directory}:{key}";
+            string value = LocExtension.GetLocalizedValue<string>(fullKey);
+            return value ?? fullKey;
+        }
 
         public static void SwitchLanguage(string languageCode = "zh-CN")
         {
@@ -16,18 +33,6 @@ namespace PvzLauncherRemake.Utils.Configuration
             Thread.CurrentThread.CurrentUICulture = newCulture;
 
             LocalizeDictionary.Instance.Culture = newCulture;
-        }
-
-        public static string GetLoc(string key)
-        {
-            return LocExtension.GetLocalizedValue<string>(key) ?? $"Key.{key}";
-        }
-
-        public static string GetLoc(string directory, string key)
-        {
-            string fullKey = $"{AssemblyName}:{directory}:{key}";
-            string value = LocExtension.GetLocalizedValue<string>(fullKey);
-            return value ?? fullKey;
         }
     }
 }
