5c6adb68696530ba3010148a1d99a5e7600961a1
HuaMouRen <2825338723@qq.com>
2026-08-02 08:38:45 +0800

    imp(download): 下载缓存文件名改为Guid生成，而非之前的随机数生成

--- src/PvzLauncherRemake/Utils/Services/GameManager.cs
+++ src/PvzLauncherRemake/Utils/Services/GameManager.cs
@@ -348,7 +348,7 @@ namespace PvzLauncherRemake.Utils.Services
 
         public static async Task StartDownloadAsync(JsonDownloadIndex.GameInfo info, string savePath, bool isTrainer)
         {
-            string tempPath = Path.Combine(Globals.Directories.TempDiectory, $"PVZLAUNCHER.DOWNLOAD.CACHE.{new Random().Next(Int32.MinValue, Int32.MaxValue) + new Random().Next(Int32.MinValue, Int32.MaxValue)}");
+            string tempPath = Path.Combine(Globals.Directories.TempDiectory, $"PVZLAUNCHER.DOWNLOAD.CACHE.{Guid.NewGuid():N}");
 
 
 
