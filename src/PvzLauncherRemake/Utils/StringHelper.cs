5809f12885011d3c281e47ea688d8823b5459956
HuaMouRen <2825338723@qq.com>
2026-08-29 11:32:35 +0800

    imp(string-helper): 拆分字符集

--- src/PvzLauncherRemake/Utils/StringHelper.cs
+++ src/PvzLauncherRemake/Utils/StringHelper.cs
@@ -4,22 +4,47 @@ namespace PvzLauncherRemake.Utils
 {
     public class StringHelper
     {
-        public List<string> RandomStrings = new List<string>()
+        public static class RandomeStrings
         {
-            "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",
-            "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M",
-            "1","2","3","4","5","6","7","8","9","0","-","=","!","@","#","$","%","^","&","*","(",")","_","+","[","]",
-            "\\",";","'",",",".","/","{","}","|",":","\"","<",">","?"
-        };
+            public static readonly List<string> All = new List<string>()
+            {
+                "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",
+                "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M",
+                "1","2","3","4","5","6","7","8","9","0","-","=","!","@","#","$","%","^","&","*","(",")","_","+","[","]",
+                "\\",";","'",",",".","/","{","}","|",":","\"","<",">","?"
+            };
+            public static readonly List<string> Lowercases = new List<string>()
+            {
+                "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"
+            };
+            public static readonly List<string> Capitals = new List<string>()
+            {
+                "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"
+            };
+            public static readonly List<string> Letters = new List<string>()
+            {
+                "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",
+                "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"
+            };
+            public static readonly List<string> Numbers = new List<string>()
+            {
+                "1","2","3","4","5","6","7","8","9","0"
+            };
+            public static readonly List<string> Symbols = new List<string>()
+            {
+                "-","=","!","@","#","$","%","^","&","*","(",")","_","+","[","]","\\",";","'",",",".","/","{","}","|",":","\"","<",">","?"
+            };
+        }
+
 
         private Random _random = new Random();
 
-        public string GenRandomeString(int length)
+        public string GenRandomeString(int length, List<string> dict)
         {
             var sb = new StringBuilder();
             for (int i = 0; i < length; i++)
             {
-                sb.Append(RandomStrings[_random.Next(0, RandomStrings.Count)]);
+                sb.Append(dict[_random.Next(0, dict.Count)]);
             }
             return sb.ToString();
         }
