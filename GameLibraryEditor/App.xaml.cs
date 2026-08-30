8a83ca362cdd7be619a9bcd1551f548d4ae0d716
HuaMouRen <2825338723@qq.com>
2026-08-30 10:23:06 +0800

    Initialize

---
+++ GameLibraryEditor/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GameLibraryEditor.Windows;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GameLibraryEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var win = new WindowMain();
+            this.MainWindow = win;
+            win.Show();
+        }
+    }
+
+}
