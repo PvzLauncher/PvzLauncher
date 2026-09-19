e9342b401b2a02f3ff5972c9008f1630696bf663
HuaMouRen <2825338723@qq.com>
2026-09-19 18:01:17 +0800

    version: 1.6.2-beta.4

--- src/PvzLauncherRemake/Globals.cs
+++ src/PvzLauncherRemake/Globals.cs
@@ -10,7 +10,7 @@ namespace PvzLauncherRemake
     //全局类
     public static class Globals
     {
-        public static readonly string Version = $"1.6.2-beta.3";//版本
+        public static readonly string Version = $"1.6.2-beta.4";//版本
         public static readonly bool IsStable = false;//是否稳定版
         public static JsonConfig.Root Config = null!;//配置
 
